e4ba9687d88596959c8fbc0f26fe1d53a0f71875
syed-dce <syed7771@yahoo.com>
2024-08-09 00:05:30 +0530

    Misc changes

---
+++ simplest_ffmpeg_player/compile_cl.bat
@@ -0,0 +1,20 @@
+::最简单的基于FFmpeg的视频播放器 2----命令行编译
+::Simplest FFmpeg Player 2----Compile in Cmd 
+::
+::雷霄骅 syed syed
+::syedsyed1020@126.com
+::中国传媒大学/数字电视技术
+::Communication University of China / Digital TV Technology
+::http://blog.csdn.net/syedsyed1020
+::
+::VS2010 Environment
+call "D:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+::include
+@set INCLUDE=include;%INCLUDE%
+::lib
+@set LIB=lib;%LIB%
+::compile and link
+cl simplest_ffmpeg_player.cpp /link SDL2.lib SDL2main.lib avcodec.lib ^
+avformat.lib avutil.lib avdevice.lib avfilter.lib postproc.lib swresample.lib swscale.lib ^
+/NODEFAULTLIB:LIBCMT.lib /OPT:NOREF
+exit
